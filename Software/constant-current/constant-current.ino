656cd4bb2a36cd2b0b2d0ba4152295d98a9e2acc
Adrian Bowyer <adrian@reprappro.com>
2022-03-18 11:30:55 +0000

    Final (?) version of constant current source.

--- Software/constant-current/constant-current.ino
+++ Software/constant-current/constant-current.ino
@@ -1,26 +1,46 @@
-
+/*
+ * Programmable constant current source.
+ * 
+ * Adrian Bowyer
+ * Reprap Ltd.
+ * 
+ * https://reprapltd.com
+ * 
+ * Licence: GPL 3
+ * 
+ */
 
 // Compile with Heltek WiFi Kit 32
 
-#include <U8g2lib.h>  
 
+// OLED graphics
+
+#include <U8g2lib.h> 
 char chBuffer[128];
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, 16, 15, 4);         // OLED graphics
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, 16, 15, 4);
 
-const int aPin = A0;
+const int aInPin = A5;      // Voltage read
+const int aOutPin = 25;     // Voltage out
+const int upPin = 13;       // Raise current
+const int downPin = 12;     // Lower current
+const int controlPin = 14;  // On/off
+const int minV = 100;       // Lowest effective voltage (100/255)
+const float r = 6.3;        // Ohms. Resistor in Darlington emitter follower
+const float m = 1.181;      // Empirical current correction...
+const float c = -4.5;       // ... Iout = m*Iin + c
+
+int vD = minV;               // Voltage fed into the base of the Darlington n/255
+float voltage = 0;          // V and ...
+float current = 0;          // ... I now
 
 void setup()
 {
-  pinMode(aPin, INPUT);
+  pinMode(aInPin, INPUT);
+  pinMode(aOutPin, OUTPUT);
+  pinMode(upPin, INPUT_PULLUP);
+  pinMode(downPin, INPUT_PULLUP);
+  pinMode(controlPin, INPUT);
   
-  // Serial.
-  
-  Serial.begin(115200);
-  while(!Serial)
-  {
-    Serial.print('.');
-  }
-
   // OLED graphics.
   
   u8g2.begin();
@@ -31,20 +51,50 @@ void setup()
   u8g2.setFontDirection(0);
 
   u8g2.clearBuffer();
-  sprintf(chBuffer, "%s", "Starting up...");
+  sprintf(chBuffer, "%s", "Const. I starting...");
   u8g2.drawStr(64 - (u8g2.getStrWidth(chBuffer) / 2), 0, chBuffer);
   u8g2.sendBuffer();
   delay(1000);
 }
 
+ 
 void loop()
 {
+  // Current on?
+  
+  if(!digitalRead(controlPin))
+  {
+    // No
+    
+    dacWrite(aOutPin, minV);
+  }else
+  {
+    // Yes
+    
+    if(!digitalRead(downPin))
+      vD -= 10;
+    if(!digitalRead(upPin))
+      vD += 10;
+    if(vD < minV)
+      vD = minV;
+    if(vD > 255)
+      vD = 255;
+    dacWrite(aOutPin, vD);
+  }
+
+  // Measure the current
+  
+  voltage = (float)map(analogRead(aInPin), 0, 3000, 150, 2450);
+  current = voltage/r;
+  current = m*current + c;
+
+  // Display it
+  
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_crox5h_tf);
-  int mv = map (analogRead(aPin), 0, 3000, 150, 2450);
-  sprintf(chBuffer, "%d mV", mv);
-  Serial.println(chBuffer);
+  u8g2.setFont(u8g2_font_crox5h_tf);  
+  sprintf(chBuffer, "%.2f mA", current); 
   u8g2.drawStr(10, 10, chBuffer);
   u8g2.sendBuffer();
+  
   delay(1000);
 }
